cd8c3de79433c52e1c2da77b5ec1ed1531db0383
Stephen Pariñas <npar276@aucklanduni.ac.nz>
2025-03-18 20:19:04 +1300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">Stephen Parinas</h1>
+            <p className="text-lg text-gray-700 mt-2">Software Engineer at Tidy International</p>
+        </div>
+    );
+}
