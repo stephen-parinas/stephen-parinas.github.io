343a3cca6ef829a07021238f414d7f5426026747
Stephen Pariñas <npar276@aucklanduni.ac.nz>
2025-03-20 01:39:08 +1300

    set homepage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "https://stephen-parinas.github.io",
+    base: "/",
     plugins: [react()],
 })
